a0f015e4c9d2933cfdb2d021d1c36c35142f65f3
quietcorey <shelton.ca@gmail.com>
2017-03-15 13:23:41 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='brochat-bot',
+    version='0.0.1',
+    packages=[''],
+    url='',
+    license='MIT',
+    author='quietcorey',
+    author_email='shelton.ca@gmail.com',
+    description='A bot for bros.'
+)
