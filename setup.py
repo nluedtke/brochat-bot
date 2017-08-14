b96568872beb425cc5c6f50eb5f22e2d1eb9e22d
nluedtke <nicholas.luedtke@uwalumni.com>
2017-08-14 13:40:53 -0600

    Add discord.py to setup reqs

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setup(
     license='MIT',
     author='quietcorey',
     author_email='shelton.ca@gmail.com',
-    description='A bot for bros.', requires=['twython', 'twilio']
+    description='A bot for bros.', requires=['twython', 'twilio', 'discord.py']
 )
