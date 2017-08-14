f86b13c3961c35e06cff6d897a9242391f1f5422
Nicholas Luedtke <nicholas.luedtke@uwalumni.com>
2017-08-14 07:39:14 -0600

    Key items on id rather than name
    This will keep the data base in duel_item class. And then we can just
    add the item and number of uses to a player's inventory.

--- duel_item.py
+++ duel_item.py
@@ -3,12 +3,13 @@
 from random import choice
 
 # Define a common item here.
-# The key is the name of the item
+# The key is the id of the item
 # The item is a dictionary with two key:value pairs
 #   1) type: effect_type
 #   2) prop: strength or effect discripter
 #   3) uses: amount of uses, measured in duels
 #   4) text: text description of item
+#   5) name: name of item
 # effect_type: effect
 # Currently there are three effect_types:
 #   1) roll_effect = This effect modifies a roll
@@ -16,31 +17,35 @@ from random import choice
 #   3) spec_effect = This effect does something different other than the two
 #           above and takes places after rolls go into effect.
 common_items = {
-    "Copper Ring of One Better": {"type": "roll_effect",
-                                  "prop": 1,
-                                  "uses": 1,
-                                  "text": "This ring adds +1 to all damage "
-                                          "for the user for one duel."},
-    "Bronze Ring of One Better": {"type": "roll_effect",
-                                  "prop": 1,
-                                  "uses": 2,
-                                  "text": "This ring adds +1 to all damage "
-                                          "for the user for two duels."},
-    "Steel Ring of One Better": {"type": "roll_effect",
-                                 "prop": 1,
-                                 "uses": 4,
-                                 "text": "This ring adds +1 to all damage "
-                                         "for the user for four duels."}
+    "0": {"name": "Copper Ring of One Better",
+          "type": "roll_effect",
+          "prop": 1,
+          "uses": 1,
+          "text": "This ring adds +1 to all damage "
+                  "for the user for one duel."},
+    "1": {"name": "Bronze Ring of One Better",
+          "type": "roll_effect",
+          "prop": 1,
+          "uses": 2,
+          "text": "This ring adds +1 to all damage "
+                  "for the user for two duels."},
+    "2": {"name": "Steel Ring of One Better",
+          "type": "roll_effect",
+          "prop": 1,
+          "uses": 4,
+          "text": "This ring adds +1 to all damage "
+                  "for the user for four duels."}
 }
 
 # Rare items go here and generally considered be more powerful either in
 # strength or duration
 rare_items = {
-    "Gold Ring of One Better": {"type": "roll_effect",
-                                "prop": 1,
-                                "uses": 10,
-                                "text": "This ring adds +1 to all damage "
-                                        "for the user for ten duels."}
+    "100": {"name": "Gold Ring of One Better",
+            "type": "roll_effect",
+            "prop": 1,
+            "uses": 10,
+            "text": "This ring adds +1 to all damage "
+                    "for the user for ten duels."}
 }
 
 
@@ -62,13 +67,15 @@ class DuelItem(object):
 
         if 5 >= item_roll > 1:
             item = choice(list(common_items.keys()))
-            self.name = item
+            print("Created " + item)
+            self.name = common_items[item]['name']
             self.prop = common_items[item]['prop']
             self.type = common_items[item]['type']
             self.uses = common_items[item]['uses']
         elif item_roll == 1:
             item = choice(list(rare_items.keys()))
-            self.name = item
+            print("Created " + item)
+            self.name = rare_items[item]['name']
             self.prop = rare_items[item]['prop']
             self.type = rare_items[item]['type']
             self.uses = rare_items[item]['uses']
