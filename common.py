37e4af355f1882157e1a841981333afb7a70c28a
Nicholas Luedtke <nicholas.luedtke@uwalumni.com>
2017-08-30 18:57:07 -0600

    Cap drinks banked at 5
    Encouraging Friendship.

--- common.py
+++ common.py
@@ -67,18 +67,3 @@ def add_drink(user):
         user['drinks_owed'] = 1
 
     return user['drinks_owed']
-
-
-def consume_drink(user):
-    """
-    Consumes a drink for the user.
-    :param user:
-    :return:
-    """
-
-    if "drinks_owed" in user:
-        user['drinks_owed'] -= 1
-    else:
-        user['drinks_owed'] = -1
-
-    return user['drinks_owed']
