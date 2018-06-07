bbae6d29db2ca4ad80f746b8ad90d1577c47f3e6
Nicholas Luedtke <nicholas.luedtke@uwalumni.com>
2018-06-07 06:18:42 -0400

    cap self damage of hit miss items

--- cogs/duelcog.py
+++ cogs/duelcog.py
@@ -871,7 +871,7 @@ async def event_handle_shot_duel(ctx, victim):
                         if c_roll > 4:
                             c_roll = c_item.prop['damage']
                         elif c_roll < 1:
-                            c_roll = -1 * c_item.prop['damage']
+                            c_roll = -1 * c_item.prop['damage'] / 2
                         else:
                             c_roll = 0
 
@@ -888,7 +888,7 @@ async def event_handle_shot_duel(ctx, victim):
                         if v_roll > 4:
                             v_roll = v_item.prop['damage']
                         elif v_roll < 1:
-                            v_roll = -1 * v_item.prop['damage']
+                            v_roll = -1 * v_item.prop['damage'] / 2
                         else:
                             v_roll = 0
 
